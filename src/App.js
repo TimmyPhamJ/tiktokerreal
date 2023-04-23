2f9759c86108979e36b08eefd190492c224e7211
Timmy Phạm <minhthatra@gmail.com>
2023-04-23 12:54:47 +0700

    1
    2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { Fragment } from 'react';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
   return (
-    <div className="App">
-      <h1>React app</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Layout = route.layout === null ? Fragment : DefaultLayout;
+            const Page = route.component;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
