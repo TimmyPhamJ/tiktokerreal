efdf22dc9308d481fb83425b01c3e4d63d2605f6
Timmy Phạm <minhthatra@gmail.com>
2023-04-24 14:16:14 +0700

    1/2 baif Ui 2
    2

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,30 +1,60 @@
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import Tippy from '@tippyjs/react/headless';
 
 import styles from './Header.module.scss';
 
 import images from '~/assets/images';
 
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+
 const cx = classNames.bind(styles);
 
 function Header() {
+  const [searchResult, setSearchResult] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([1, 2, 3]);
+    }, 3000);
+  }, []);
+
   return (
     <header className={cx('wrapper')}>
       <div className={cx('inner')}>
         <div className={cx('logo')}>
           <img src={images.logo} alt="TikTok" />
         </div>
-        <div className={cx('search')}>
-          <input placeholder="Search accounts and videos" spellCheck={false} />
-          <button className={cx('clear-btn')}>
-            <FontAwesomeIcon icon={faCircleXmark} />
-          </button>
-          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-          <button className={cx('search-btn')}>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
-        </div>
+
+        <Tippy
+          interactive
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+            <PopperWrapper>
+              <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                Kết Quả
+              </div>
+            </PopperWrapper>
+          )}
+        >
+          <div className={cx('search')}>
+            <input placeholder="Search accounts and videos" spellCheck={false} />
+
+            <button className={cx('clear-btn')}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+
+            <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+            <button className={cx('search-btn')}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
+
         <div className={cx('actions')}></div>
       </div>
     </header>
---
+++ src/components/Popper/Wrapper.js
@@ -0,0 +1,11 @@
+import classNames from 'classnames/bind';
+
+import styles from './Popper.module.scss';
+
+const cx = classNames.bund(styles);
+
+function Wrapper({ children }) {
+  return <div className={cx('wapper')}>{children}</div>;
+}
+
+export default Wrapper;
