c1cd9e08bd4275742984fd2272b899f265e725e7
Timmy Phạm <minhthatra@gmail.com>
2023-04-30 01:45:22 +0700

    Xây dựng Logic phần Header #1
    :D search

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -3,12 +3,14 @@ import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
+import Image from '~/components/Image';
+
 const cx = classNames.bind(styles);
 
 function AccountItem() {
   return (
     <div className={cx('wrapper')}>
-      <img
+      <Image
         className={cx('avatar')}
         src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/4abd3d07ddea0f1c636aec1bba9cf997~c5_100x100.jpeg?x-expires=1682524800&x-signature=7L6g4kvIdlbeqZBQAkzAKSg4iOA%3D"
         alt="Roisee"
