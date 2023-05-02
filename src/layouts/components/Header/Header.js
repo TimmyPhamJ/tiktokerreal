b152b6b6f2a4c957212993aabe7ef32af007b148
Timmy Phạm <minhthatra@gmail.com>
2023-05-02 21:43:28 +0700

    Tối Ưu file index trong dự án
    đổi tên file trung w thư mục và thêm 1 file index để export default ra

---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,266 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEllipsisVertical,
+  faEarthAsia,
+  faCircleQuestion,
+  faKeyboard,
+  faUser,
+  faCoins,
+  faGear,
+  faSignOut,
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import 'tippy.js/dist/tippy.css';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css';
+
+import config from '~/config';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
+import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
+import Image from '~/components/Image';
+import Search from '../Search';
+
+const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+  {
+    icon: <FontAwesomeIcon icon={faEarthAsia} />,
+    title: 'English',
+    children: {
+      title: 'Language',
+      data: [
+        {
+          type: 'tiếng anh',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'Vietnamess',
+          code: 'vi',
+          title: 'Tiếng Việt',
+          children: {
+            title: 'Vùng Miềng',
+            data: [
+              {
+                code: 'vin',
+                title: 'Tiếng Việt Bắc',
+              },
+              {
+                code: 'vis',
+                title: 'Tiếng Việt Nam',
+              },
+            ],
+          },
+        },
+        {
+          type: 'tiếng nhật',
+          code: 'JP',
+          title: 'Japaness',
+        },
+        {
+          type: 'tiếng trung',
+          code: 'cn',
+          title: 'Tàu Khựa',
+        },
+        {
+          type: 'tiếng anh',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'Vietnamess',
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+        {
+          type: 'tiếng nhật',
+          code: 'JP',
+          title: 'Japaness',
+        },
+        {
+          type: 'tiếng trung',
+          code: 'cn',
+          title: 'Tàu Khựa',
+        },
+        {
+          type: 'tiếng anh',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'Vietnamess',
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+        {
+          type: 'tiếng nhật',
+          code: 'JP',
+          title: 'Japaness',
+        },
+        {
+          type: 'tiếng trung',
+          code: 'cn',
+          title: 'Tàu Khựa',
+        },
+        {
+          type: 'tiếng anh',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'Vietnamess',
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+        {
+          type: 'tiếng nhật',
+          code: 'JP',
+          title: 'Japaness',
+        },
+        {
+          type: 'tiếng trung',
+          code: 'cn',
+          title: 'Tàu Khựa',
+        },
+        {
+          type: 'tiếng anh',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'Vietnamess',
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+        {
+          type: 'tiếng nhật',
+          code: 'JP',
+          title: 'Japaness',
+        },
+        {
+          type: 'tiếng trung',
+          code: 'cn',
+          title: 'Tàu Khựa',
+        },
+        {
+          type: 'tiếng anh',
+          code: 'en',
+          title: 'English',
+        },
+        {
+          type: 'Vietnamess',
+          code: 'vi',
+          title: 'Tiếng Việt',
+        },
+      ],
+    },
+  },
+  {
+    icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+    title: 'Feedback and Help',
+    to: '/feedback',
+  },
+  {
+    icon: <FontAwesomeIcon icon={faKeyboard} />,
+    title: 'Keyboard Shortcuts',
+  },
+];
+
+function Header() {
+  const currentUser = true;
+
+  //Handle logic
+  const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+      case 'Vietnamess':
+        //Handle change language
+        break;
+      default:
+    }
+  };
+
+  const userMenu = [
+    {
+      icon: <FontAwesomeIcon icon={faUser} />,
+      title: 'View profile',
+      to: '/@thanhmeo.18',
+    },
+    {
+      icon: <FontAwesomeIcon icon={faCoins} />,
+      title: 'Get coins',
+      to: '/coins',
+    },
+    {
+      icon: <FontAwesomeIcon icon={faGear} />,
+      title: 'Setting',
+      to: '/setting',
+    },
+    ...MENU_ITEMS,
+    {
+      icon: <FontAwesomeIcon icon={faSignOut} />,
+      title: 'Log out',
+      to: '/logout',
+      separate: true,
+    },
+  ];
+
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        <Link to={config.routes.home} className={cx('logo-link')}>
+          <img src={images.logo} alt="TikTok" />
+        </Link>
+
+        <Search />
+
+        <div className={cx('actions')}>
+          {currentUser ? (
+            <>
+              <Tippy delay={[0, 200]} content="Upload Video" placement="bottom">
+                <button className={cx('action-btn')}>
+                  <UploadIcon />
+                </button>
+              </Tippy>
+              <Tippy delay={[0, 200]} content="Message" placement="bottom">
+                <button className={cx('action-btn')}>
+                  <MessageIcon />
+                </button>
+              </Tippy>
+              <Tippy delay={[0, 200]} content="Upload Video" placement="bottom">
+                <button className={cx('action-btn')}>
+                  <InboxIcon />
+                </button>
+              </Tippy>
+            </>
+          ) : (
+            <>
+              <Button text>Upload</Button>
+              <Button primary>Log in</Button>
+            </>
+          )}
+
+          <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+            {currentUser ? (
+              <Image
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/33fd8341eec3c69dc34972b27738b860~c5_100x100.jpeg?x-expires=1682920800&x-signature=V%2B%2FvdiQnIJYhg%2Bjq7Nt3zij%2BGcQ%3D"
+                className={cx('user-avatar')}
+                alt="ThaoHentai"
+                //fallback="http://files.fullstack.edu.vn/user_avatars/623b4b2d95cec.png"
+              />
+            ) : (
+              <button trigger="click" className={cx('more-btn')}>
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </button>
+            )}
+          </Menu>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/layouts/components/Search/Search.js
@@ -0,0 +1,156 @@
+import { useEffect, useState, useRef } from 'react';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import HeadlessTippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+
+import * as searchService from '~/services/searchService';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+import { SearchIcon } from '~/components/Icons/Icons';
+import { useDebounce } from '~/hook';
+import styles from './Search.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Search() {
+  const [searchValue, setSearchValue] = useState('');
+
+  const [searchResult, setSearchResult] = useState([]);
+
+  const [showResult, setshowResult] = useState(true);
+  const [loading, reloading] = useState(false);
+
+  const debounced = useDebounce(searchValue, 800);
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (!debounced.trim()) {
+      setSearchResult([]);
+      return;
+    }
+
+    const fetchApi = async () => {
+      reloading(true);
+
+      const result = await searchService.search(debounced);
+      setSearchResult(result);
+
+      reloading(false);
+    };
+
+    fetchApi();
+
+    /*
+    reloading(true);
+
+    const fetchApi = async () => {
+      try {
+        const res = await request.get('users/search', {
+          params: {
+            q: debounced,
+            type: 'less',
+          },
+        });
+        setSearchResult(res.data);
+        reloading(false);
+      } catch (error) {
+        reloading(false);
+      }
+    };
+
+    fetchApi();*/
+  }, [debounced]);
+
+  /*fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
+      .then((res) => res.json())
+      .then((res) => {
+        setSearchResult(res.data);
+        reloading(false);
+      })
+      .catch(() => {
+        reloading(false);
+      });
+  }, [debounced]);*/
+
+  /*request
+      .get('users/search', {
+        params: {
+          q: debounced,
+          type: 'less',
+        },
+      })
+      .then((res) => {
+        setSearchResult(res.data);
+        reloading(false);
+      })
+      .catch(() => {
+        reloading(false);
+      });
+  }, [debounced]);*/
+
+  const handleClear = () => {
+    setSearchValue('');
+    setSearchResult([]);
+    inputRef.current.focus();
+  };
+
+  const handleHideResult = () => {
+    setshowResult(false);
+  };
+
+  const handleChange = (e) => {
+    const searchValue = e.target.value;
+
+    if (!searchValue.startsWith(' ')) {
+      setSearchValue(searchValue);
+    }
+  };
+
+  return (
+    //Using a wrapper <div> or <span> tag around the reference element solves this by creating a new parentNode context.
+    <div>
+      <HeadlessTippy
+        interactive
+        visible={showResult && searchResult.length > 0}
+        render={(attrs) => (
+          <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+            <PopperWrapper>
+              <h4 className={cx('search-title')}>Accounts</h4>
+              {searchResult.map((result) => (
+                <AccountItem key={result.id} data={result} />
+              ))}
+            </PopperWrapper>
+          </div>
+        )}
+        onClickOutside={handleHideResult}
+      >
+        <div className={cx('search')}>
+          <input
+            ref={inputRef}
+            value={searchValue}
+            placeholder="Search accounts and videos"
+            spellCheck={false}
+            onChange={handleChange}
+            onFocus={() => setshowResult(true)}
+          />
+
+          {!!searchValue && !loading && (
+            <button className={cx('clear-btn')} onClick={handleClear}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+          )}
+
+          {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+
+          <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()}>
+            <SearchIcon />
+          </button>
+        </div>
+      </HeadlessTippy>
+    </div>
+  );
+}
+
+export default Search;
